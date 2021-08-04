a665cdb3c1fa3e2cc0324a215c0ac513ffdf2c1a
Lachlan Kemp <me@lachlankemp.com>
2021-08-04 19:33:42 +0930

    added some sort of authorisation

---
+++ components/authbutton.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+export default function AuthButton() {
+  const [inputVisible, setInputVisible] = useState(false)
+  const [userName, setUserName] = useState("")
+
+  function handleChange(event) {
+    const input = event.target;
+    const value = input.type === 'checkbox' ? input.checked : input.value;
+  
+    setUserName(value)
+  }
+  function handleLogIn() {
+    localStorage.setItem('user', userName);
+  }
+
+    return <>
+      {!inputVisible && 
+        <button onClick={() => {
+          setInputVisible(true)
+        }}>Join</button>
+      }
+
+      {inputVisible &&
+        <div>
+          <span>Enter Name </span>
+          <input placeholder="BC Crew" onChange={handleChange}/>
+          <button onClick={handleLogIn}>GO!</button>
+        </div>
+      }
+    </>
+}
+
+
+
