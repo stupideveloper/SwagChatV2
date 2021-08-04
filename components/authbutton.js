ea313afde84f2614c9d0896dab9529a95ea083cf
Lachlan Kemp <me@lachlankemp.com>
2021-08-04 20:08:23 +0930

    added some temporary styling

--- components/authbutton.js
+++ components/authbutton.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router'
+import styles from '../styles/Button.module.css'
 
 export default function AuthButton() {
   const [inputVisible, setInputVisible] = useState(false)
   const [userName, setUserName] = useState("")
   const router = useRouter()
-
 
   function handleChange(event) {
     const input = event.target
@@ -14,14 +14,14 @@ export default function AuthButton() {
     setUserName(value)
   }
   function handleLogIn() {
-    localStorage.setItem('user', userName);
+    localStorage.setItem('user', userName)
 
     router.push('chat')
   }
 
     return <>
       {!inputVisible && 
-        <button onClick={() => {
+        <button className={styles.button} onClick={() => {
           setInputVisible(true)
         }}>Join</button>
       }
